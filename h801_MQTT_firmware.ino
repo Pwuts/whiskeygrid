1eb8b85876a25e0ba403e7a4d0a03abd9fbdc15d
Reinier van der Leer <reintjebadboy@gmail.com>
2016-08-10 23:47:46 +0200

    First working version!

--- h801_MQTT_firmware.ino
+++ h801_MQTT_firmware.ino
@@ -23,10 +23,18 @@ const char* mqtt_server = "192.168.1.31";
 const char* ssid    = "whiskeygrid";
 const char* wpa2key = "7A6U6QM0RQ0Z";
 
+int rgb[3] = {0,0,0}; //Variable in which RGB values are stored, handy for when powered off
+
 void setup() {
   //Serial.begin(115200);
   //Serial.println("Initializing..");
-  pinMode(led, OUTPUT);
+  pinMode(redFET, OUTPUT);
+  pinMode(greenFET, OUTPUT);
+  pinMode(blueFET, OUTPUT);
+  pinMode(w1FET, OUTPUT);
+  pinMode(w2FET, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
   WiFi.begin(ssid, wpa2key);
   client.setServer(mqtt_server, 1883);
   client.setCallback(mqttCallback);
@@ -34,6 +42,8 @@ void setup() {
 
 void loop() {
   if (!client.connected()) {
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
     reconnect();
   }
   client.loop();
@@ -48,6 +58,8 @@ void reconnect() {
     //Serial.println(reconnectAttempts);
     if(client.connect(chipid)) { //try to reconnect, check if succeeds
       //Serial.println("connected"); //debug info --> serial
+      digitalWrite(redLed, LOW);
+      digitalWrite(greenLed, HIGH);
       client.publish("whiskeygrid/debug", chipid); //debug info --> MQTT
       client.subscribe("whiskeygrid/#"); //resubscribe to listen for incoming messages again
     } else {
@@ -63,6 +75,7 @@ void reconnect() {
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
   char bericht[50] = "";
+  digitalWrite(greenLed, LOW);
  
   //Serial.print("Hee ik hoor wat in [");
   //Serial.print(topic);
@@ -80,14 +93,18 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
   // Some topics (buttons for example) don't need a specific payload handled, just a reaction to the topic. Saves a lot of time!
 
   if (strcmp(topic, "whiskeygrid/rgb/1") == 0 || strcmp(topic, "whiskeygrid/rgb") == 0) {
-    if (strcmp(bericht, "aan") == 0) {
-      digitalWrite(led, HIGH);
-      //Serial.println("lampje aan :)");
+    if (sscanf(bericht, "rgb(%d,%d,%d", rgb, rgb + 1, rgb + 2) == 3 || strcmp(bericht, "aan") == 0) {
+      analogWrite(redFET, rgb[0]);
+      analogWrite(greenFET, rgb[1]);
+      analogWrite(blueFET, rgb[2]);
     }
     if (strcmp(bericht, "uit") == 0) {
-      digitalWrite(led, LOW);
+      digitalWrite(redFET, LOW);
+      digitalWrite(greenFET, LOW);
+      digitalWrite(blueFET, LOW);
       //Serial.println("lampje uit :(");
     }
   }
+  digitalWrite(greenLed, HIGH);
 }
 
