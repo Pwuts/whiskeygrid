a75a61e17775719909e23817cd0663a80787713a
Reinier van der Leer <reintjebadboy@gmail.com>
2020-05-16 01:06:52 +0200

    added ClimateNode-DHT22-MHZ19B; updated README;

--- ClimateNode-DHT22-MHZ19B/ClimateNode-DHT22-MHZ19B.ino
+++ ClimateNode-DHT22-MHZ19B/ClimateNode-DHT22-MHZ19B.ino
@@ -5,13 +5,20 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <DHT.h>
+#include <MHZ19.h>
+#include <SoftwareSerial.h>
 
 #define Sprintf(f, ...) ({ char* s; asprintf(&s, f, __VA_ARGS__); String r = s; free(s); r; })
 
 #define DHTPIN D4
 #define DHTTYPE DHT22
 
-DHT sensor(DHTPIN, DHTTYPE);
+DHT dht22(DHTPIN, DHTTYPE);
+
+#define MHZ19_TX D1
+#define MHZ19_RX D2
+SoftwareSerial mhzSerial(MHZ19_RX, MHZ19_TX);
+MHZ19 mhz19;
 
 char* d_mqtt_host = "127.0.0.1";
 int   d_mqtt_port = 1883;
@@ -28,18 +35,22 @@ String mqtt_root;
 String node_name;
 String temperature_topic;
 String RH_topic;
+String co2_topic;
 String connect_topic;
 String call_topic;
-int interval;
+int dht22_interval;
+int mhz19_interval;
 int temp_offset;
 
 bool influx_enabled;
-String influx_topic, influx_temperature_measurement, influx_humidity_measurement;
+String influx_topic, influx_temperature_measurement, influx_humidity_measurement, influx_co2_measurement;
 
 /* Global variables/instances */
 
 unsigned long last_measurement = 0;
 float temp_c, humidity;
+int co2_ppm;
+float mhz19_temp;
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
@@ -61,7 +72,13 @@ void setup() {
     mqttClient.setServer(mqtt_host.c_str(), mqtt_port);
     // mqttClient.setCallback(mqtt_callback);
 
-    sensor.begin();     // initialize DHT22
+    // initialize temperature/humidity sensor
+    dht22.begin();
+
+    // initialize CO2 sensor
+    mhzSerial.begin(9600);
+    mhz19.begin(mhzSerial);
+    mhz19.autoCalibration();
 }
 
 bool first_measurement = true;
@@ -78,12 +95,12 @@ void loop() {
         WiFiSettings.portal();
     }
 
-    if (!(millis() % interval) || first_measurement == true){
+    if (!(millis() % dht22_interval) || first_measurement == true) {
         digitalWrite(LED_BUILTIN, LOW);
 
         /* Reading temperature for humidity takes about 250 milliseconds!
          * Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor) */
-        get_measurement();
+        dht22_get_measurement();
 
         // Temperature -> MQTT topic
         mqtt_publish(temperature_topic, Sprintf("%.1f °C", temp_c).c_str(), true);
@@ -101,15 +118,37 @@ void loop() {
         digitalWrite(LED_BUILTIN, HIGH);
     }
 
+    if (!(millis() % mhz19_interval) || first_measurement == true) {
+        digitalWrite(LED_BUILTIN, LOW);
+
+        /* Get latest temperature and CO2 readings from MH-Z19 sensor */
+        mhz19_get_measurement();
+
+        // CO2 -> MQTT topic
+        mqtt_publish(co2_topic, Sprintf("%d ppm", co2_ppm).c_str(), true);
+
+        // CO2 -> Influx
+        influx_publish(influx_co2_measurement, Sprintf("co2=%d", co2_ppm), "sensor=\"MH-Z19\"");
+
+        // // Temperature -> MQTT topic
+        // mqtt_publish(temperature_topic, Sprintf("%.1f °C"), true);
+
+        // Temperature -> Influx
+        influx_publish(influx_temperature_measurement, Sprintf("temperature=%.1f", mhz19_temp), "sensor=\"MH-Z19\"");
+
+        Serial.println();
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+
     if (first_measurement)  first_measurement = false;
 }
 
 
-void get_measurement() {
+void dht22_get_measurement() {
     // Reading temperature for humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-    temp_c = sensor.readTemperature() + temp_offset;  // Read temperature as Celcius
-    humidity = sensor.readHumidity();  // Read humidity (percent)
+    temp_c = dht22.readTemperature() + temp_offset;  // Read temperature as Celcius
+    humidity = dht22.readHumidity();  // Read humidity (percent)
 
     Serial.printf("DHT22: %.1f°C\r\n", temp_c);
     Serial.printf("DHT22: %.1f%%RH\r\n", humidity);
@@ -119,6 +158,19 @@ void get_measurement() {
         Serial.println("Failed to read from DHT sensor!");
         return;
     }
+}
+
+void mhz19_get_measurement() {
+    int i = 0;
+    do {    // retry until good reading
+        co2_ppm = mhz19.getCO2();
+        delay(1); i++;
+    } while (co2_ppm == 0);
+
+    mhz19_temp = mhz19.getTemperature(true);
+
+    Serial.printf("MH-Z19: %d ppm (attempt %d)\r\n", co2_ppm, i);
+    Serial.printf("MH-Z19: %.1f°C\r\n", mhz19_temp);
 }
 
 
@@ -144,11 +196,11 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
     Serial.printf("Message arrived on topic: [%s], %s\r\n", topic, message.c_str());
 
     // if(message == node_name + ", temperature, c"){
-    //     get_measurement();
+    //     dht22_get_measurement();
 
     //     mqtt_publish(temperature_topic, Sprintf("%.1f °C", temp_c).c_str(), true);
     // } else if (message == node_name + ", humidity"){
-    //     get_measurement();
+    //     dht22_get_measurement();
 
     //     mqtt_publish(RH_topic, Sprintf("%.1f %%RH", humidity).c_str(), true);
     // }
@@ -168,18 +220,24 @@ void setup_wifi() {
 
     String d_temp_topic = "/temperature/" + node_name;
     String d_RH_topic   = "/humidity/" + node_name;
+    String d_co2_topic  = "/co2/" + node_name;
     temperature_topic = WiFiSettings.string("mqtt-temperature-topic", d_temp_topic, F("MQTT temperature topic"));
     RH_topic          = WiFiSettings.string("mqtt-humidity-topic",    d_RH_topic,   F("MQTT RH topic"));
+    co2_topic         = WiFiSettings.string("mqtt-co2-topic",         d_co2_topic,  F("MQTT CO2 topic"));
 
     influx_enabled                 = WiFiSettings.checkbox("influx-enabled",               false,            F("Enable influx"));
     influx_topic                   = WiFiSettings.string("influx-topic",                   d_influx_topic,   F("Influx MQTT topic"));
     influx_temperature_measurement = WiFiSettings.string("influx-temperature-measurement", F("temperature"), F("Temperature influx measurement name"));
     influx_humidity_measurement    = WiFiSettings.string("influx-humidity-measurement",    F("humidity"),    F("Humidity influx measurement name"));
+    influx_co2_measurement         = WiFiSettings.string("influx-co2-measurement",         F("co2"),         F("CO2 influx measurement name"));
 
-    interval    = WiFiSettings.integer("measure-interval",   10, 3600, 30, F("Measuring interval [s] (shorter = less accurate)"));
-    temp_offset = WiFiSettings.integer("temperature-offset", -5, 5,     0, F("Temperature offset [°C]"));
+    dht22_interval = WiFiSettings.integer("dht22-measure-interval", 10, 3600, 30, F("Measuring interval [s] for DHT22 sensor (shorter = less accurate)"));
+    temp_offset    = WiFiSettings.integer("temperature-offset",     -5, 5,     0, F("Temperature offset [°C] for DHT22"));
 
-    interval *= 1000;   // seconds -> milliseconds
+    mhz19_interval = WiFiSettings.integer("mhz19-measure-interval",  1, 3600, 10, F("Measuring interval [s] for MH-Z19 sensor"));
+
+    dht22_interval *= 1000;   // seconds -> milliseconds
+    mhz19_interval *= 1000;
 
     WiFiSettings.onPortal = []() {
         setup_ota();
--- README.md
+++ README.md
@@ -3,15 +3,43 @@ whiskeygrid
 How I got by the name, I have no idea. But this project is focused on rigging my
 home with sensors and automating all kinds of stuff.
 
-## Arduino sketches
 These sketches are all written for ESP8266-based hardware. I personally use a lot of
-Wemos D1 mini v2's, but anything else based off of an ESP8266 should work, and with
+Wemos D1 mini v2's, but anything else based off of an ESP8266 *should* work, and with
 some modification these will probably also run on other platforms.
 
-### DHT2MQTT
-This sketch runs on a Wemos D1 mini with a DHT22 temperature and relative humidity
-sensor. It regularly measures temperature and RH and reports to the specified
-channels on MQTT.
+## ClimateNodes
+The ClimateNodes are supposed to provide different kinds of climate and air quality
+data in similar formats. They communicate via MQTT, and in case of sensors, they all
+have integrated support for influx formatting. This means they can publish metrics
+to a specific topic in influx line format, allowing you to pass it directly into
+InfluxDB, e.g. using something like Telegraf.
 
-# WiFi-MQTT-LED-controller
-It accepts messages in the format of rgb(X,Y,Z) where XYZ can be anything from 0 to 255, and 1, 2 or 3 digits. Patches welcome! :)
+### DHT2MQTT
+**Hardware:**
+* Wemos D1 mini
+* DHT22 temperature & RH sensor
+
+**Output:**
+* Temperature [°C]
+* Relative Humidity [%]
+
+### ClimateNode-DHT22-MHZ19
+**Hardware:**
+* Wemos D1 mini
+* DHT22 temperature & RH sensor
+* MH-Z19B CO2 sensor
+
+**Output:**
+* Temperature [°C]
+* Relative Humidity [%]
+* CO2 concentration [ppm]
+* Temperature (MH-Z19B) [°C] (influx only)
+
+## SwitchNodes
+Things to switch and control other things.
+
+### WiFi-MQTT-LED-controller
+This sketch powers a WiFi and MQTT-connected LED controller. It accepts messages in
+the format of rgb(X,Y,Z) where XYZ can be anything from 0 to 255, and 1, 2 or 3 digits.
+I'm working on adding CCT support and NTP, and I'm still trying to think of a way to
+control all the RGBW LED goodness that I'm going to be wiring my walls and ceiling with.
